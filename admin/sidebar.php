e7d73059fd29c4b05fce4432aaf1d3a9c2ab9bf5
Mohammad <162719285+moded12@users.noreply.github.com>
2025-05-17 15:40:59 +0300

    Delete admin directory

--- admin/sidebar.php
+++
@@ -1,18 +0,0 @@
-<?php
-// FILE: admin/sidebar.php
-?>
-<div class="sidebar">
-  <h4>📘 المعلم الإلكتروني</h4>
-  <p class="small">مرحبًا admin - اختر من القائمة</p>
-  <a href="add_application.php">📱 التطبيقات</a>
-  <a href="add_class.php">📚 الصفوف</a>
-  <a href="add_material.php">📘 المواد</a>
-  <a href="add_semester.php">🕓 الفصول</a>
-  <a href="add_section.php">🏷️ الأقسام</a>
-  <a href="add_group.php">📂 المجموعات</a>
-  <a href="add_lessons.php">📖 الدروس</a>
-  <a href="add_attachment.php">📎 الملفات</a>
-  <a href="add_lessons.php">✏️ تعديل وحذف الدروس</a>
-  <a href="manage_slugs.php">🔍 Slugs إدارة</a>
-  <a href="../logout.php" class="text-danger">🚪 تسجيل الخروج</a>
-</div>
